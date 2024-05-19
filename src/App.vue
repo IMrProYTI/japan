b1ab5270ccb838a3687312a5ed1d5447cba59f75
Alexander <s555b@mail.ru>
2024-05-20 01:29:21 +0300

    Remove commented navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <nav class="absolute space-x-2 text-white">
-    <router-link to="/">Home</router-link>
-    <router-link to="/admin">Admin</router-link>
-    <router-link to="/overlay/POTI">Player</router-link>
-  </nav> -->
   <router-view />
 </template>
 

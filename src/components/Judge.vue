156438aa74d0a28d52b3b0c39598069cf0d43cf9
Alexander <s555b@mail.ru>
2024-05-20 11:18:53 +0300

    Add timer on admin tasks page and new component

--- src/components/Judge.vue
+++ src/components/Judge.vue
@@ -3,7 +3,7 @@
     <div v-if="$route.params.player" class="flex flex-col justify-center items-center">
       <div class="*:text-xl *:text-center p-2">
         <h1>Задания: {{ $route.params.player }}</h1>
-        <p>{{ `Текущее время: ${getTimer()}` }}</p>
+        <Timer />
       </div>
       <Tasks />
     </div>
@@ -11,21 +11,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref, Ref } from 'vue';
 import Tasks from './Judge/Tasks.vue';
-
-const timer: Ref<Date> = ref(new Date());
-
-function getTimer(): string {
-  const hours = `${timer.value.getHours()}`;
-  const minutes = timer.value.getMinutes() < 10 ? `0${timer.value.getMinutes()}` : `${timer.value.getMinutes()}`;
-  const seconds = timer.value.getSeconds() < 10 ? `0${timer.value.getSeconds()}` : `${timer.value.getSeconds()}`;
-  return `${hours}:${minutes}:${seconds}`;
-};
-
-setInterval(() => {
-  timer.value = new Date();
-}, 1000);
+import Timer from './root/Timer.vue';
 </script>
 
 <style scoped>
---
+++ src/components/root/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ `Текущее время: ${getTimer()}` }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+
+const timer: Ref<Date> = ref(new Date());
+
+function getTimer(): string {
+  const hours = `${timer.value.getHours()}`;
+  const minutes = timer.value.getMinutes() < 10 ? `0${timer.value.getMinutes()}` : `${timer.value.getMinutes()}`;
+  const seconds = timer.value.getSeconds() < 10 ? `0${timer.value.getSeconds()}` : `${timer.value.getSeconds()}`;
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+setInterval(() => {
+  timer.value = new Date();
+}, 1000);
+</script>
+
+<style scoped>
+
+</style>

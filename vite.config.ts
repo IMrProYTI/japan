4425a403c242ca5916d98b3aa926f34afc1fa89e
Alexander <s555b@mail.ru>
2024-05-26 20:32:33 +0300

    Fix rollup failing

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      external: ["/logo.png"]
-    }
+    outDir: "dist"
   }
 })

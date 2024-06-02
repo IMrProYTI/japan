2873cc2fc2c0fe19ceb77eef62801835b8c2a059
Alexander <s555b@mail.ru>
2024-06-02 18:16:18 +0300

    Write participant nickname

--- src/views/QRCode.vue
+++ src/views/QRCode.vue
@@ -1,6 +1,7 @@
 <template>
-	<div class="flex justify-center items-center w-full h-screen">
-		<img :src="`http://api.qrserver.com/v1/create-qr-code/?data=${uri}`" alt="">
+	<div class="flex flex-col justify-center items-center w-full h-screen space-y-2">
+		<p class="font-bold text-4xl">{{ participant }}</p>
+		<img :src="`http://api.qrserver.com/v1/create-qr-code/?data=${uri}`">
 	</div>
 </template>
 

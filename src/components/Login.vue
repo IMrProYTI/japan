63808ce4e472e10ba4c5415af7c9ec9b8e1044fc
Alexander <s555b@mail.ru>
2024-06-12 14:24:14 +0300

    Login page UI update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="flex flex-col w-screen min-h-screen">
     <div class="fixed flex justify-between w-full p-2">
-      <Link to="/">Главная</Link>
-      <router-link to="/"><Danger>Закрыть</Danger></router-link>
+      <div class="flex space-x-2">
+        <Link class="px-2 py-1" c-type="router" to="/">Главная</Link>
+        <Link class="px-2 py-1" c-type="router" to="/posts">Посты</Link>
+      </div>
+      <Mode />
     </div>
-    <div class="flex flex-1 flex-col justify-center items-center space-y-2">
-      <button
-        class="px-2 py-1 rounded border-2 border-transparent bg-neutral-100 hover:border-neutral-300 active:bg-neutral-300 dark:bg-slate-900 dark:hover:border-slate-800 dark:active:bg-slate-800"
-        @click="signIn"
-      >
-        Авторизоваться через GitHub
-      </button>
-      <p v-if="errorMessage" class="font-semibold text-red-600">{{ errorMessage }}</p>
+    <div class="flex flex-1 justify-center items-center">
+      <div class="flex flex-col justify-center items-center rounded p-4 space-y-4 bg-neutral-200 dark:bg-slate-900">
+        <img class="drop-shadow w-44" src="/logo.png" alt="logo">
+        <div class="flex flex-col justify-center items-center">
+          <h1 class="text-xl">Авторизуйтесь через <b class="font-semibold">GitHub</b></h1>
+          <p>Чтобы войти в панель администратора</p>
+        </div>
+        <hr class="w-full border-neutral-300 dark:border-slate-800">
+        <div class="flex w-full space-x-2">
+          <Common class="flex-1 px-2 py-1 dark:bg-slate-800 dark:hover:border-slate-700" @click="signIn">Авторизоватся</Common>
+          <Danger class="p-1" c-type="router" to="/">
+            <span class="material-symbols">logout</span>
+          </Danger>
+        </div>
+        <p v-if="errorMessage" class="font-semibold text-red-600">{{ errorMessage }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +32,9 @@ import { ref, Ref } from 'vue';
 import { useRoute } from 'vue-router';
 import supabase from '../supabase';
 
+import Mode from './root/Mode.vue';
 import Link from './root/Link.vue';
+import Common from './root/Common.vue';
 import Danger from './root/Danger.vue';
 
 const errorMessage: Ref<string | undefined> = ref("");

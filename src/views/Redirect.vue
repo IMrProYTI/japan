1424fbe982e3647be4032aae8548f50c887d35e3
Alexander <s555b@mail.ru>
2024-07-19 14:34:05 +0300

    Remove all non admin features from project

---
+++ src/views/Redirect.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex justify-center items-center w-screen min-h-screen bg-neutral-300 dark:text-white dark:bg-slate-950">
+		<div class="flex justify-center items-center space-x-4">
+			<div class="text-center">
+				<p class="text-2xl font-bold">Перенаправление</p>
+				<p class="w-max h-auto">Пожалуйста подождите{{ '...'.slice(0, dots) }}</p>
+			</div>
+			<Loading />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import Loading from '../components/root/Loading.vue';
+
+const dots = ref(0);
+setInterval(() => {
+	dots.value += 1;
+	dots.value %= 4;
+}, 500)
+</script>
+
+<style scoped>
+
+</style>
